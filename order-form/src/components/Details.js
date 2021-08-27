769dfb201653d02fb4378395d8289391c5e0ab3a
NaurisM <nauris.magone@gmail.com>
2021-08-27 14:01:08 +0300

    Update Details.js

--- order-form/src/components/Details.js
+++ order-form/src/components/Details.js
@@ -1,75 +1,34 @@
 import { useParams } from "react-router-dom";
 import Data from "../data.json"
 
+
+
 const Details = () => {
     const { code } = useParams();
+    const properties = Data.varieties.map((p) => p.code);
     const product = Data.items.find((item) => item.code === code);
-    const properties = Data.varieties.find((property) => property.code === "SIZE");
-    //const property = Data.varieties.map((p) => p.code);
-
-    // property.forEach((prop) => {
-    //     const attributes = Data.varieties.find((attribute) => attribute.code === {prop});
-    //         {(() => {
-    //             if (product.varieties.includes(prop)) {
-    //                 return (
-    //                     <select name={prop} id={prop}>
-    //                         {attributes.options.map((s) => (
-    //                             <option value={s.description}>{s.description}</option>
-    //                         ))}  
-    //                     </select>
-    //                 )
-    //             }
-    //         })()}
-    //     }
-    // )
-
-    const AddProperties = (prop) => {
-        const properties = Data.varieties.find((property) => property.code === prop);    
-        
-            if (product.varieties.includes(prop)) {
-            return (
-                <select name={prop} id={prop}>
-                    {properties.options.map((s) => (
-                        <option value={s.description}>{s.description}</option>
-                    ))}  
-                </select>
-            )
-        }
-    }
-
-    console.log(Data.varieties.map((p) => p.code));
     
     return ( 
         <div className="item-details">
             <h2>{ product.description }</h2>
-            {(() => {
-                if (product.varieties.includes('SIZE')) {
+            {properties.map((prop) => {
+                const property = Data.varieties.find((attribute) => attribute.code === prop);
+            
+                if (product.varieties.includes(prop)) {
                     return (
-                        <select name="size" id="size">
-                            {properties.options.map((s) => (
-                                <option value={s.description}>{s.description}</option>
-                            ))}
-                            
+                        <select name={prop} id={prop}>
+                            {property.options.map((a) => (
+                                <option value={a.description}>{a.description}</option>
+                            ))};  
                         </select>
                     )
+                }else{
+                    return null;
                 }
-            })()}
-            {(() => {
-                if (product.varieties.includes('COLOR')) {
-                    return (
-                        <div>Color exist</div>
-                    )
-                }
-            })()}
-            {(() => {
-                if (product.varieties.includes('SHOE-SIZE')) {
-                    return (
-                        <div>Shoe size exist</div>
-                    )
-                }
-            })()}
+            }
+            )}
         </div>
-     );
+    );
 }
  
 export default Details;
