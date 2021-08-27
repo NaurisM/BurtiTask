070aa619c378011f6079a1b1cfe55c910c5a5463
NaurisM <nauris.magone@gmail.com>
2021-08-27 21:00:05 +0300

    add form

--- order-form/src/components/Details.js
+++ order-form/src/components/Details.js
@@ -1,33 +1,45 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import Data from "../data.json"
 
-
-
 const Details = () => {
+    const [propState, setPropState] = useState("");
     const { code } = useParams();
     const properties = Data.varieties.map((p) => p.code);
     const product = Data.items.find((item) => item.code === code);
     
     return ( 
-        <div className="item-details">
+        <form className="item-details">
             <h2>{ product.description }</h2>
             {properties.map((prop) => {
                 const property = Data.varieties.find((attribute) => attribute.code === prop);
             
                 if (product.varieties.includes(prop)) {
                     return (
-                        <select name={prop} id={prop}>
-                            {property.options.map((a) => (
-                                <option value={a.description}>{a.description}</option>
-                            ))};  
-                        </select>
+                        <div className="choices">
+                            <h5>{property.description}</h5>
+                            <select 
+                                className="dropdown" 
+                                value={propState}
+                                onChange={(e) => {
+                                    const selectedAttr = e.target.value;
+                                    setPropState(selectedAttr);
+                                }}
+                            >
+                                {property.options.map((a) => (
+                                    <option value={a.code}>{a.description}</option>
+                                ))};
+                            </select>
+                        </div>
                     )
                 }else{
                     return null;
                 }
             }
             )}
-        </div>
+            <input type="submit" value="Pasūtīt" />
+            <h4>Preces kods: {product.code}.{propState}</h4>
+        </form>
     );
 }
  
